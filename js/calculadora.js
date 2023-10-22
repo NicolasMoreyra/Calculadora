552ac88c51673806ba9256a7c2695e66db7519b9
NicolasMoreyra <138818834+NicolasMoreyra@users.noreply.github.com>
2023-10-22 20:44:59 -0300

    cambios0

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,88 +1,81 @@
-var operacionActual = '';
-var resultado = 0;
-var operadorAnterior = '';
-var decimalFlag = false;
+document.addEventListener('DOMContentLoaded', function() {
+    const display = document.getElementById('display');
+    const buttons = document.getElementById('buttons');
+    const clearButton = document.getElementById('clear');
+    const saveButton = document.getElementById('save');
 
-function actualizarDisplay() {
-    var display = document.getElementById('display');
-    display.value = operacionActual;
-}
+    let historial = JSON.parse(localStorage.getItem('historial')) || [];
 
-function agregarNumero(numero) {
-    operacionActual += numero;
-    actualizarDisplay();
-}
-
-function agregarOperador(operador) {
-    if (operadorAnterior !== '') {
-        calcularResultado();
+    function updateDisplay(value) {
+        display.value += value;
     }
-    operadorAnterior = operador;
-    operacionActual += operador;
-    actualizarDisplay();
-    decimalFlag = false;
-}
 
-function agregarDecimal() {
-    if (!decimalFlag) {
-        operacionActual += '.';
-        actualizarDisplay();
-        decimalFlag = true;
+    function clearDisplay() {
+        display.value = '';
     }
-}
 
-function calcularResultado() {
-    resultado = eval(operacionActual);
-    operacionActual = resultado.toString();
-    operadorAnterior = '';
-    actualizarDisplay();
-    decimalFlag = false;
-}
-
-function limpiarDisplay() {
-    operacionActual = '';
-    resultado = 0;
-    operadorAnterior = '';
-    actualizarDisplay();
-    decimalFlag = false;
-}
-
-function borrarUltimoDigito() {
-    operacionActual = operacionActual.slice(0, -1);
-    actualizarDisplay();
-}
-
-document.addEventListener('keydown', function(event) {
-    switch(event.key) {
-        case '0':
-        case '1':
-        case '2':
-        case '3':
-        case '4':
-        case '5':
-        case '6':
-        case '7':
-        case '8':
-        case '9':
-            agregarNumero(parseInt(event.key));
-            break;
-        case '+':
-        case '-':
-        case '*':
-        case '/':
-            agregarOperador(event.key);
-            break;
-        case '.':
-            agregarDecimal();
-            break;
-        case 'Enter':
-            calcularResultado();
-            break;
-        case 'Delete':
-            limpiarDisplay();
-            break;
-        case 'Backspace':
-            borrarUltimoDigito();
-            break;
+    function saveToHistory(expression, result) {
+        historial.push({ expression, result });
+        localStorage.setItem('historial', JSON.stringify(historial));
     }
+
+    function handleButtonClick(value) {
+        if (value === '=') {
+            try {
+                const result = eval(display.value);
+                updateDisplay(` = ${result}`);
+                saveToHistory(display.value, result);
+            } catch (error) {
+                updateDisplay('Error');
+            }
+        } else if (value === 'C') {
+            clearDisplay();
+        } else {
+            updateDisplay(value);
+        }
+    }
+
+    function createButton(value) {
+        const button = document.createElement('button');
+        button.textContent = value;
+        button.classList.add('btn', 'btn-secondary');
+        button.addEventListener('click', function() {
+            handleButtonClick(value);
+        });
+        return button;
+    }
+
+    function loadButtons() {
+        const buttonValues = ['7', '8', '9', '/',
+                              '4', '5', '6', '*',
+                              '1', '2', '3', '-',
+                              '0', '.', '=', '+'];
+
+        buttonValues.forEach(value => {
+            const button = createButton(value);
+            buttons.appendChild(button);
+        });
+    }
+
+    loadButtons();
+
+    clearButton.addEventListener('click', function() {
+        clearDisplay();
+    });
+
+    saveButton.addEventListener('click', function() {
+        saveToHistory(display.value, eval(display.value));
+        alert('Historial guardado');
+    });
+
+    document.addEventListener('keydown', function(event) {
+        const key = event.key;
+        const validKeys = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '/',
+                           '*', '-', '+', '=', '.', 'Enter', 'Backspace'];
+
+        if (validKeys.includes(key)) {
+            event.preventDefault();
+            handleButtonClick(key);
+        }
+    });
 });
